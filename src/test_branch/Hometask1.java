0af833d04c63f4bc0d0838c4aa1963c915acc091
volohva <kurazh7@ya.ru>
2022-12-04 23:30:41 +0300

    Выполнила правки

--- src/test_branch/Hometask1.java
+++ src/test_branch/Hometask1.java
@@ -2,8 +2,8 @@ package test_branch;
 
 public class Hometask1 {
     public static void main(String[] args) {
-        System.out.println("check it out");
-        System.out.println("check it out");
+        System.out.println("check it out!");
+        System.out.println("check it out!");
         System.out.println("Пример пример");
     }
 }
